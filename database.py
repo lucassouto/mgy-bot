ec8ea6a4ef4f62fcd001ec37306a6c5d6e4c472f
Lucas Souto <lucassouto@protonmail.com>
2023-11-17 13:07:19 -0300

    feat: make sqlalchemy session async
    Should use asynchronous SQLAlchemy to avoid freezing your bot while running database queries.
    https://docs.sqlalchemy.org/en/14/orm/extensions/asyncio.html#synopsis-orm
    https://discordpy.readthedocs.io/en/stable/faq.html#what-does-blocking-mean

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 from decouple import config
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 DB_USER = config("DB_USER", default="user")
 DB_PASS = config("DB_PASS", default="pass")
 DB_NAME = config("DB_NAME", default="name")
 DB_PORT = config("DB_PORT", default="5432")
 
-engine = create_engine(f"postgresql://{DB_USER}:{DB_PASS}@127.0.0.1:{DB_PORT}/{DB_NAME}")
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@127.0.0.1:{DB_PORT}/{DB_NAME}")
+SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
