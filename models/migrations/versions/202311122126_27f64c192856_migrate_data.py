16130196fb4a1f7b7d736b058a712ce79331f7dc
Lucas Souto <lucassouto@protonmail.com>
2023-11-17 15:00:20 -0300

    feat: create migrations
    These migrations create a new structure with constraints that create a strong relationship between the models and migrate data from old tables before deleting them

---
+++ models/migrations/versions/202311122126_27f64c192856_migrate_data.py
@@ -0,0 +1,42 @@
+"""migrate_data
+
+Revision ID: 27f64c192856
+Revises: b3faf853662b
+Create Date: 2023-11-12 21:26:56.317453
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '27f64c192856'
+down_revision: Union[str, None] = 'b3faf853662b'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Migrate level data
+    op.execute("INSERT INTO levels (id, value, name, created_at, updated_at) SELECT id_niveis, id_niveis, nome_nivel, NOW(), NOW() FROM niveis")
+
+    # Migrate musics data
+    op.execute("INSERT INTO musics (id, name, url, user_id, title, created_at, updated_at) SELECT id_musicas, name, url, user_id, title, NOW(), NOW() FROM music_links")
+
+    # Migrate servers data
+    op.execute("INSERT INTO servers (id, name, discord_id, created_at, updated_at) SELECT id_servidores, nome_servidor, numero_id_servidor, NOW(), NOW() FROM servidores")
+
+    # Migrate users data
+    op.execute(
+        "INSERT INTO users (id, name, discord_user_id, level_id, experience, server_id, youtube, twitch, twitter, others, total_messages, created_at, updated_at) "
+        "SELECT id_usuarios, nome_usuario, user_id_discord, nivel_id, CAST(experiencia AS INTEGER), servidor_id, youtube, twitch, twitter, outros, total_mensagens, NOW(), NOW() " 
+        "FROM usuarios"
+    )
+
+def downgrade() -> None:
+    op.execute("DELETE FROM levels")
+    op.execute("DELETE FROM musics")
+    op.execute("DELETE FROM servers")
+    op.execute("DELETE FROM users")
---
+++ models/migrations/versions/202311122127_7d95a89af124_remove_old_tables.py
@@ -0,0 +1,60 @@
+"""remove_old_tables
+
+Revision ID: 7d95a89af124
+Revises: 27f64c192856
+Create Date: 2023-11-12 21:27:27.620053
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '7d95a89af124'
+down_revision: Union[str, None] = '27f64c192856'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.drop_table('niveis')
+    op.drop_table('servidores')
+    op.drop_table('usuarios')
+    op.drop_table('music_links')
+
+
+def downgrade() -> None:
+    op.create_table('usuarios',
+    sa.Column('id_usuarios', sa.INTEGER(), autoincrement=True, nullable=False),
+    sa.Column('nome_usuario', sa.VARCHAR(), autoincrement=False, nullable=False),
+    sa.Column('user_id_discord', sa.VARCHAR(), autoincrement=False, nullable=False),
+    sa.Column('nivel_id', sa.INTEGER(), autoincrement=False, nullable=False),
+    sa.Column('experiencia', sa.VARCHAR(length=45), server_default=sa.text('0'), autoincrement=False, nullable=False),
+    sa.Column('servidor_id', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=False),
+    sa.Column('youtube', sa.VARCHAR(length=200), autoincrement=False, nullable=True),
+    sa.Column('twitch', sa.VARCHAR(length=200), autoincrement=False, nullable=True),
+    sa.Column('twitter', sa.VARCHAR(length=200), autoincrement=False, nullable=True),
+    sa.Column('outros', sa.VARCHAR(length=2000), autoincrement=False, nullable=True),
+    sa.Column('total_mensagens', sa.INTEGER(), server_default=sa.text('0'), autoincrement=False, nullable=True),
+    sa.ForeignKeyConstraint(['servidor_id'], ['servidores.id_servidores'], name='fk_numero_id_servidor'),
+    sa.PrimaryKeyConstraint('id_usuarios', name='usuarios_pkey')
+    )
+    op.create_table('servidores',
+    sa.Column('id_servidores', sa.INTEGER(), autoincrement=True, nullable=False),
+    sa.Column('nome_servidor', sa.VARCHAR(), autoincrement=False, nullable=False),
+    sa.Column('numero_id_servidor', sa.VARCHAR(), autoincrement=False, nullable=False),
+    sa.PrimaryKeyConstraint('id_servidores', name='servidores_pkey')
+    )
+    op.create_table('niveis',
+    sa.Column('id_niveis', sa.INTEGER(), autoincrement=False, nullable=False),
+    sa.Column('nome_nivel', sa.VARCHAR(length=255), autoincrement=False, nullable=False)
+    )
+    op.create_table('music_links',
+    sa.Column('id_musicas', sa.INTEGER(), autoincrement=False, nullable=False),
+    sa.Column('name', sa.VARCHAR(length=255), autoincrement=False, nullable=False),
+    sa.Column('url', sa.VARCHAR(length=255), autoincrement=False, nullable=False),
+    sa.Column('user_id', sa.INTEGER(), autoincrement=False, nullable=False),
+    sa.Column('title', sa.VARCHAR(length=255), autoincrement=False, nullable=False),
+    )
